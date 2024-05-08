d838a4f2111ed01161f191236d0a5a5e80dc86b8
md-imran97 <mdimran.ml109@gmail.com>
2024-05-09 04:23:44 +0600

    Update Home.jsx

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,200 +1,52 @@
 import React from "react";
 import { Splitter, SplitterPanel } from "primereact/splitter";
+import { Panel } from "primereact/panel";
+import { InputTextarea } from "primereact/inputtextarea";
 
 const Home = () => {
   return (
     <div className="mx-2 my-4 bg-primary">
-      <Splitter style={{ height: "400px", borderRadius: "0px" }}>
+      <Splitter style={{ height: "420px", borderRadius: "0px" }}>
         <SplitterPanel className=" " size={65} minSize={50}>
-          <div className=" flex flex-row flex-wrap   overflow-auto h-full">
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              1
+          <Panel
+            header="Visualize"
+            className="overflow-auto h-full w-full"
+            pt={{ content: { style: { border: "0px" } } }}
+          >
+            <div className=" flex flex-row flex-wrap  ">
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
+                1
+              </div>
+
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
+                3
+              </div>
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
+                3
+              </div>
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
+                3
+              </div>
+
+              <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
+                3
+              </div>
             </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              2
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              1
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              2
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              1
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              2
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              1
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              2
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-            <div className="flex align-items-center justify-content-center w-4rem h-4rem bg-primary font-bold border-round m-2">
-              3
-            </div>
-          </div>
+          </Panel>
         </SplitterPanel>
-        <SplitterPanel
-          className="flex align-items-center justify-content-center"
-          size={35}
-          minSize={10}
-        >
-          Panel 2
+        <SplitterPanel className="" size={35} minSize={10}>
+          <Panel
+            header="Code"
+            className="overflow-auto h-full w-full"
+            pt={{ content: { style: { border: "0px" } } }}
+          >
+            <InputTextarea
+              autoResize
+              className="w-full"
+              //onChange={(e) => setValue(e.target.value)}
+              rows={15}
+            />
+          </Panel>
         </SplitterPanel>
       </Splitter>
     </div>
